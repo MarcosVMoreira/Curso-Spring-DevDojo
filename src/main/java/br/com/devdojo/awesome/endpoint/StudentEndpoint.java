1e0b849c45a3e28d8af01421e557e9d2e6cc818d
Marcos <contato.marcosvmoreira@gmail.com>
2020-03-04 22:04:49 -0300

    Add: Primeira API Rest

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,4 +1,22 @@
 package br.com.devdojo.awesome.endpoint;
 
+import br.com.devdojo.awesome.model.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+//RestController, em comparacao com Controller, adiciona automaticamente a notacao ResponseBody para transformar para JSON
+@RestController
+@RequestMapping("student")
 public class StudentEndpoint {
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll() {
+        return asList(new Student("Deku"), new Student("Todoroki"));
+    }
+
+
 }
--- src/main/java/br/com/devdojo/awesome/start/ApplicationStart.java
+++ src/main/java/br/com/devdojo/awesome/start/ApplicationStart.java
@@ -1,4 +1,17 @@
 package br.com.devdojo.awesome.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+//passa uma ideia pro spring do que ele tem que configurar com base nos componentes que temos
+//o componenteScan passa onde está o controller, já que nao está na mesma pasta do ApplicationStart. Sem ele, a página nao consegue achar os endpoins e retorna 404 qnd a gente acessa pelo navegador
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "br.com.devdojo.awesome.endpoint")
 public class ApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+
 }
